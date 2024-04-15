2eb3e59e4b87549ba24cebc4b6a4d9db43be8472
Ginger <75683114+gingerindustries@users.noreply.github.com>
2024-04-15 16:08:37 +0000

    Fix various reflection issues in Builtins

--- src/main/kotlin/pcl/Builtins.kt
+++ src/main/kotlin/pcl/Builtins.kt
@@ -6,32 +6,34 @@ import kotlin.reflect.KCallable
 import kotlin.reflect.typeOf
 import kotlin.reflect.full.memberFunctions
 import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.isSubtypeOf
+import kotlin.reflect.full.valueParameters
 
 object Builtins {
     @Suppress("UNCHECKED_CAST")
     val builtins = Builtins::class.memberFunctions.filter {
-        it.returnType == typeOf<List<StackValue<*>>>()
+        it.returnType.isSubtypeOf(typeOf<List<StackValue<*>>>())
     }.groupBy { it.name }.mapValues { (name, overloads) ->
-        val takesCallingFunction = overloads.first().parameters.first().name!! == "callingFunction"
-        val arity = overloads.first().parameters.size
+        val takesCallingFunction = overloads.first().valueParameters.first().name!! == "callingFunction"
+        val arity = overloads.first().valueParameters.size
         if (takesCallingFunction) {
-            check(overloads.all { it.parameters.first().let { it.name!! == "callingFunction" && it.type == typeOf<Function>() } }) {
+            check(overloads.all { it.valueParameters.first().let { it.name!! == "callingFunction" && it.type == typeOf<Function>() } }) {
                 "All overloads for builtin function $name must take a callingFunction parameter if any one does!"
             }
         }
-        check(overloads.all { it.parameters.size == arity }) {
+        check(overloads.all { it.valueParameters.size == arity }) {
             "All overloads for builtin function $name must have the same number of parameters!"
         }
         BuiltinFunction(
             name, arity, takesCallingFunction,
             overloads.map { overload ->
-                overload.parameters.map { param ->
+                overload.valueParameters.map { param ->
                     val paramType = param.type.classifier!! as? KClass<*>
                         ?: throw IllegalStateException()
                     when (paramType) {
-                        typeOf<Double>() -> StackValue.Number::class
-                        typeOf<String>() -> StackValue.String::class
-                        typeOf<Function>() -> StackValue.Function::class
+                        Double::class -> StackValue.Number::class
+                        String::class -> StackValue.Str::class
+                        Function::class -> StackValue.Function::class
                         else -> error("Builtin function ${overload.name} has invalid type ${paramType.qualifiedName} for parameter ${param.name}!")
                     }
                 } to overload as KFunction<List<StackValue<*>>>
