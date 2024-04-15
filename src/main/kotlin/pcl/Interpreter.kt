4fb4a49df158da8d9c9d12a67acf071c71d70b7d
Ginger <75683114+gingerindustries@users.noreply.github.com>
2024-04-15 16:20:26 +0000

    Fix pluralization mistakes in error messages

--- src/main/kotlin/pcl/Interpreter.kt
+++ src/main/kotlin/pcl/Interpreter.kt
@@ -14,20 +14,20 @@ object Interpreter {
                     val builtin = Builtins.builtins[node.name]
                         ?: throw BuiltinException(node.range, "Cannot call unknown builtin ${node.name}!")
                     if (function.stack.size < builtin.arity) {
-                        throw BuiltinException(node.range, "Cannot call builtin ${node.name} with ${function.stack.size} items on the stack! (needs at least ${builtin.arity})")
+                        throw BuiltinException(node.range, "Cannot call builtin ${node.name} with ${function.stack.size} ${if (function.stack.size == 1) "item" else "items"} on the stack! (needs at least ${builtin.arity})")
                     }
 
                     val args = function.stack.pop(builtin.arity)
                     val argTypes = args.map { it::class }
                     val impl = builtin.overloads[argTypes]
-                        ?: throw BuiltinException(node.range, "Builtin ${builtin.name} has no overload for arguments of type (${argTypes.map { it.simpleName }.joinToString(", ")})!")
+                        ?: throw BuiltinException(node.range, "Builtin ${builtin.name} has no overload for ${if (argTypes.size == 1) "argument" else "arguments"} of type (${argTypes.map { it.simpleName }.joinToString(", ")})!")
                     val argArray = (args.map { it.value } + function.takeIf { builtin.takesCallingFunction }).filterNotNull().toTypedArray()
 
                     impl.runCatching { call(Builtins, *argArray) }.onFailure {
                         if (it is PclException) {
                             throw it
                         } else {
-                            throw BuiltinInvokeException(node.range, "An internal exception occured in builtin ${node.name}", it as? Exception)
+                            throw BuiltinInvokeException(node.range, "An internal exception occured in builtin ${node.name}!", it as? Exception)
                         }
                     }.onSuccess {
                         function.stack.addAll(it)
@@ -47,7 +47,7 @@ object Interpreter {
     }
 }
 
-fun <T> MutableList<T>.pop(amount: Int = 1) = (0..<amount).map { removeAt(size - 1) }
+fun <T> MutableList<T>.pop(amount: Int = 1) = (0..<amount).map { removeAt(size - 1) }.reversed()
 
 data class Function(val body: List<Node>, val stack: MutableList<StackValue<*>>, val parent: Function?)
 
