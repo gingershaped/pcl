34b85f518f65639af02b7358967c2ba923d6d53f
Ginger <75683114+gingerindustries@users.noreply.github.com>
2024-04-15 16:09:06 +0000

    Add initial interpreter implementation

--- src/main/kotlin/pcl/Exceptions.kt
+++ src/main/kotlin/pcl/Exceptions.kt
@@ -1,10 +1,12 @@
 package pcl
 
-open class PclException(val range: IntRange, message: String) : Exception(message) {
+open class PclException(val range: IntRange, message: String, cause: Exception? = null) : Exception(message, cause) {
     override fun toString() = "${this::class.simpleName}: $message"
     fun highlight(program: String) =
         program + "\n" + " ".repeat(range.start) + "^" + "~".repeat(range.endInclusive - range.start)
 }
 
 class ParseException(range: IntRange, message: String) : PclException(range, message)
+class BuiltinException(range: IntRange, message: String) : PclException(range, message)
+class BuiltinInvokeException(range: IntRange, message: String, cause: Exception?) : PclException(range, message, cause)
 class TypeException(range: IntRange, message: String) : PclException(range, message)
--- src/main/kotlin/pcl/Interpreter.kt
+++ src/main/kotlin/pcl/Interpreter.kt
@@ -1,19 +1,59 @@
 package pcl
 
-class Interpreter {
-    fun run(body: List<Node>) {
-        run(Function(body, mutableListOf(), null))
+
+object Interpreter {
+    fun run(body: List<Node>): List<StackValue<*>> {
+        val stack = mutableListOf<StackValue<*>>()
+        run(Function(body, stack, null))
+        return stack
     }
     fun run(function: Function) {
-        
+        for (node in function.body) {
+            when (node) {
+                is Node.Identifier -> {
+                    val builtin = Builtins.builtins[node.name]
+                        ?: throw BuiltinException(node.range, "Cannot call unknown builtin ${node.name}!")
+                    if (function.stack.size < builtin.arity) {
+                        throw BuiltinException(node.range, "Cannot call builtin ${node.name} with ${function.stack.size} items on the stack! (needs at least ${builtin.arity})")
+                    }
+
+                    val args = function.stack.pop(builtin.arity)
+                    val argTypes = args.map { it::class }
+                    val impl = builtin.overloads[argTypes]
+                        ?: throw BuiltinException(node.range, "Builtin ${builtin.name} has no overload for arguments of type (${argTypes.map { it.simpleName }.joinToString(", ")})!")
+                    val argArray = (args.map { it.value } + function.takeIf { builtin.takesCallingFunction }).filterNotNull().toTypedArray()
+
+                    impl.runCatching { call(Builtins, *argArray) }.onFailure {
+                        if (it is PclException) {
+                            throw it
+                        } else {
+                            throw BuiltinInvokeException(node.range, "An internal exception occured in builtin ${node.name}", it as? Exception)
+                        }
+                    }.onSuccess {
+                        function.stack.addAll(it)
+                    }
+                }
+                is Node.Number -> {
+                    function.stack.add(StackValue.Number(node.value))
+                }
+                is Node.Str -> {
+                    function.stack.add(StackValue.Str(node.value))
+                }
+                is Node.Function -> {
+                    function.stack.add(StackValue.Function(Function(node.body, mutableListOf(), function)))
+                }
+            }
+        }
     }
 }
+
+fun <T> MutableList<T>.pop(amount: Int = 1) = (0..<amount).map { removeAt(size - 1) }
 
 data class Function(val body: List<Node>, val stack: MutableList<StackValue<*>>, val parent: Function?)
 
 sealed class StackValue<out T> {
     abstract val value: T
     data class Number(override val value: Double) : StackValue<Double>()
-    data class String(override val value: String) : StackValue<String>()
+    data class Str(override val value: String) : StackValue<String>()
     data class Function(override val value: pcl.Function): StackValue<pcl.Function>()
 }
